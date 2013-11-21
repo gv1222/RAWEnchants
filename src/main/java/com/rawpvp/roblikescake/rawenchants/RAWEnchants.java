4f75b852048adfdc2a41a4334496d6fbd945baf7
roblikescake <roblik3cak3@gmail.com>
2013-11-20 23:25:50 -0500

    Added Blindness Enchantment

--- src/main/java/com/rawpvp/roblikescake/rawenchants/RAWEnchants.java
+++ src/main/java/com/rawpvp/roblikescake/rawenchants/RAWEnchants.java
@@ -1,6 +1,9 @@
 package com.rawpvp.roblikescake.rawenchants;
 
+import com.rawpvp.roblikescake.rawenchants.enchants.BlindnessEnchantment;
+
 import com.rit.sucy.EnchantPlugin;
+import com.rit.sucy.EnchantmentAPI;
 
 public class RAWEnchants extends EnchantPlugin {
 
@@ -10,5 +13,9 @@ public class RAWEnchants extends EnchantPlugin {
 
 	public void onDisable() {
 		getLogger().info(this.getDescription().getName() + " " + this.getDescription().getVersion() + " has been Disabled!");
+	}
+
+	public void registerEnchantments() {
+		EnchantmentAPI.registerCustomEnchantment(new BlindnessEnchantment());
 	}
 }
---
+++ src/main/java/com/rawpvp/roblikescake/rawenchants/enchants/BlindnessEnchantment.java
@@ -0,0 +1,38 @@
+package com.rawpvp.roblikescake.rawenchants.enchants;
+
+import com.rit.sucy.CustomEnchantment;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.Random;
+
+public class BlindnessEnchantment extends CustomEnchantment {
+	static final Material[] BLINDNESS_ITEMS = new Material[] {
+			Material.WOOD_SWORD,
+			Material.STONE_SWORD,
+			Material.IRON_SWORD,
+			Material.GOLD_SWORD,
+			Material.DIAMOND_SWORD };
+
+	public BlindnessEnchantment() {
+		super("Blindness", BLINDNESS_ITEMS, 6);
+		description = "Blinds enemy on hit";
+		this.max = 3;
+		this.base = 1;
+		this.interval = 4;
+	}
+
+	@Override
+	public void applyEffect(LivingEntity user, LivingEntity target, int enchantLevel, EntityDamageByEntityEvent event) {
+		Random random = new Random();
+		int diceRoll = random.nextInt(100);
+		int effectChance = (enchantLevel * 3);
+
+		if (diceRoll <= effectChance) {
+			target.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 40, 2));
+		}
+	}
+}
