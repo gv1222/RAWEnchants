cb1ef60a827a368d9cb304665752d6b86b24b23e
roblikescake <roblik3cak3@gmail.com>
2013-11-20 23:09:31 -0500

    Loggers for days

--- src/main/java/com/rawpvp/roblikescake/rawenchants/RAWEnchants.java
+++ src/main/java/com/rawpvp/roblikescake/rawenchants/RAWEnchants.java
@@ -5,10 +5,11 @@ import com.rit.sucy.EnchantPlugin;
 public class RAWEnchants extends EnchantPlugin {
 
 	public void onEnable() {
-
+		getLogger().info(this.getDescription().getName() + " " + this.getDescription().getVersion() + " has been Enabled!");
 	}
 
 	public void onDisable() {
+		getLogger().info(this.getDescription().getName() + " " + this.getDescription().getVersion() + " has been Disabled!");
 
 	}
 }
