229403e62523ab7b87ec59f743fe01959aa6d2e1
roblikescake <roblik3cak3@gmail.com>
2013-11-20 19:13:47 -0500

    Initial Commit

---
+++ src/main/java/net/simplicite_mc/roblikescake/rawenchants/RAWEnchants.java
@@ -0,0 +1,9 @@
+package net.simplicite_mc.roblikescake.rawenchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RAWEnchants extends JavaPlugin{
+	public static RAWEnchants p;
+
+
+}
