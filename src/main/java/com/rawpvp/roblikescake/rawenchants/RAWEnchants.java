191ae8855af25d7e2eb76a44fa184f1e76f019d7
roblikescake <roblik3cak3@gmail.com>
2013-11-20 20:49:30 -0500

    Don't use my domain, use rawpvp.com domain!

---
+++ src/main/java/com/rawpvp/roblikescake/rawenchants/RAWEnchants.java
@@ -0,0 +1,14 @@
+package com.rawpvp.roblikescake.rawenchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RAWEnchants extends JavaPlugin {
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+}
--- src/main/java/net/simplicite_mc/roblikescake/rawenchants/RAWEnchants.java
+++
@@ -1,9 +0,0 @@
-package net.simplicite_mc.roblikescake.rawenchants;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class RAWEnchants extends JavaPlugin{
-	public static RAWEnchants p;
-
-
-}
